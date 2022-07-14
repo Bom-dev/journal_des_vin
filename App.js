6108844713a4f7114adb07e070074757aafaa7be
Bom <11377ny@gmail.com>
2022-07-14 11:56:48 -0400

    Set basic structure for react native app

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function Home({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Browse Wines"
+        onPress={() => navigation.navigate('WineList')}
+      />
+      {/* <Button
+        title="Browse Winemakers"
+        onPress={() => navigation.navigate('WinemakerList')}
+      /> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function WineList({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Wine List</Text>
+      <Button
+        title="Wine Detail"
+        onPress={() => navigation.navigate('WineDetail')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+}
+
+function WineDetail({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Wine Detail</Text>
+      <Button
+        title="Back to List"
+        onPress={() => navigation.navigate('WineList')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+}
+
+
+// function WinemakerList({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Winemaker List</Text>
+//       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+//       <Button title="Go back" onPress={() => navigation.goBack()} />
+//       <Button
+//         title="Go back to first screen in stack"
+//         onPress={() => navigation.popToTop()}
+//       />
+//     </View>
+//   );
+// }
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} options={{ title: 'Home' }} />
+        <Stack.Screen name="WineList" component={WineList} options={{ title: 'Wines' }} />
+        <Stack.Screen name="WineDetail" component={WineDetail} options={{title: 'Wine Detail'}} />
+        {/* <Stack.Screen name="WinemakerList" component={WinemakerList} options={{ title: 'Winemakers'}} /> */}
+        {/* <Stack.Screen name="WinemakerDetail" component={WinemakerDetail} options={{title: 'Winemaker Detail'}} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
